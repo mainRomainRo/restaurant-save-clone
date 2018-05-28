d82376390d15f86cecb289d1d6c37edf9ea2c8e9
DaTikh <baptiste.rogeon@gmail.com>
2018-05-28 11:01:20 +0200

    merge template

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-  
-=======
   root 'pages#index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> aa127275e6adecc23d87bdd8c6fb27dc959873a3
 end
