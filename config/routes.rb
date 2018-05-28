6004aa5024b5313d19c63610ab8337da42077f42
DaTikh <baptiste.rogeon@gmail.com>
2018-05-28 17:49:22 +0200

    create Carts controller, add set_cart method

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   resources :items
+  get :mon_panier, to: "carts#show", as: :mon_panier
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
