100be208aae92c7b6f8319213d4f4d3767b3a7af
iMassim0 <i.max@live.fr>
2018-05-31 19:39:55 +0200

    dependent: :destroy added on all dependency has_many

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,6 @@ class Cart < ApplicationRecord
 
   belongs_to :user
   has_many :cart_items, dependent: :destroy
-  has_many :added_items, through: :cart_items
+  has_many :added_items, through: :cart_items, dependent: :destroy
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,14 +5,14 @@ class Item < ApplicationRecord
   validates :title, :price, :description, presence: true
 
   has_many :cart_items, foreign_key: :added_item_id, dependent: :destroy
-  has_many :carts, through: :cart_items
+  has_many :carts, through: :cart_items, dependent: :destroy
 
-  has_many :users, through: :carts
+  has_many :users, through: :carts, dependent: :destroy
 
   has_many :item_orders, foreign_key: :purchased_item_id, dependent: :destroy
-  has_many :orders, through: :item_orders
+  has_many :orders, through: :item_orders, dependent: :destroy
 
-  has_many :users, through: :orders
+  has_many :users, through: :orders, dependent: :destroy
 
   attr_accessor :item_pic
 
