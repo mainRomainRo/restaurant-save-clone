57fa23ef476cece1d1f194abca42d90386c6a158
mainRomainRo <romain.fiorotto@gmail.com>
2018-06-09 16:03:28 +0200

    modificiation de l'envoie de l'email de preparation commande valide ok

--- app/mailers/chaton_mailer.rb
+++ app/mailers/chaton_mailer.rb
@@ -38,7 +38,7 @@ class ChatonMailer < ApplicationMailer
       @order = params[:order]
       @total = params[:total]
       p "Sending mail now ..."
-      mail(to: "romain.fiorotto@gmail.fr", subject: @subject)
+      mail(to: "romain.fiorotto@gmail.com", subject: @subject)
       p "Mail to admin sent."
     else
       p "something wrong here!"
